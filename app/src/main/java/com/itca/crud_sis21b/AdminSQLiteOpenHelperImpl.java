200d4084a31783f942cded85a48ae0887d680b72
raquel343 <65417681+raquel343@users.noreply.github.com>
2021-09-02 16:28:27 -0600

    cambio del producto

--- app/src/main/java/com/itca/crud_sis21b/AdminSQLiteOpenHelperImpl.java
+++ app/src/main/java/com/itca/crud_sis21b/AdminSQLiteOpenHelperImpl.java
@@ -2,13 +2,20 @@ package com.itca.crud_sis21b;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 
-public class AdminSQLiteOpenHelperImpl extends AdminSQLiteOpenHelper {
+public class AdminSQLiteOpenHelperImpl extends SQLiteOpenHelper implements View.OnClickListener {
     public AdminSQLiteOpenHelperImpl(@Nullable @org.jetbrains.annotations.Nullable Context context, @Nullable @org.jetbrains.annotations.Nullable String name, @Nullable @org.jetbrains.annotations.Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
+
+    private EditText et_codigo, et_descripcio, et_precio;
+    private Button btnalta, btnconsultar, btnconsulta2, btneliminar, btnmodificar, btnnuevo, btnsalir;
 
     @Override
     public void onCreate(SQLiteDatabase db) {
@@ -20,5 +27,13 @@ public class AdminSQLiteOpenHelperImpl extends AdminSQLiteOpenHelper {
         db.execSQL("drop table if exists articulos");
         onCreate(db);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        Switch (view.getId()){
+
+
+        }
     }
 }
